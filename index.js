6a7240edd2d5bc9959982ddbeb5422980a4a0c61
Geoffrey Bauduin <bauduin.geo@gmail.com>
2014-11-26 10:41:24 +0100

    2 routes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 * @Author: Geoffrey Bauduin <bauduin.geo@gmail.com>
 * @Date:   2014-11-26 10:22:45
 * @Last Modified by:   Geoffrey Bauduin
-* @Last Modified time: 2014-11-26 10:33:04
+* @Last Modified time: 2014-11-26 10:41:13
 */
 
 (function () {
@@ -18,14 +18,16 @@
 
 	app.use(function (req, res, next) {
 		res.setHeader("Access-Control-Allow-Origin", "*");
-		res.setHeader("Access-Control-Allow-Methods", "GET");
+		res.setHeader("Access-Control-Allow-Methods", "GET, POST");
 		res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,Content-Type");
 		res.setHeader("Access-Control-Allow-Credentials", true);
 		res.setHeader("Content-Type", "application/json");
 		next();
 	});
 
-	app.get("/api/opentok", function (req, res) {
+	var lastSession = {};
+
+	app.post("/api/opentok", function (req, res) {
 		opentok.createSession(function (error, OTSession) {
 			if (error) {
 				Logger.error(error);
@@ -34,13 +36,25 @@
 				})
 			}
 			else {
-				res.status(200).send({
+				lastSession = {
 					sessionId: OTSession.sessionId,
 					token: opentok.generateToken(OTSession.sessionId),
 					apiKey: apiKey
-				})
+				}
+				res.status(200).send(lastSession)
 			}
 		})
+	})
+
+	app.get("/api/opentok", function (req, res) {
+		if (lastSession.sessionId) {
+			res.status(200).send(lastSession);
+		}
+		else {
+			res.status(403).send({
+				error: "must POST /api/opentok before GET /api/opentok"
+			})
+		}
 	})
 
 	var server = app.listen(9876, function () {
